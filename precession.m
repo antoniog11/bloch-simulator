81817cccf09db0981b68e11a6055abf0ff008e36
antoniog11 <gl3nn0717@gmail.com>
2024-11-13 20:05:56 -0500

    Added Pulse Sequence Functionality
    I expanded the ODE solver function and added logic to enable a train of
    B1 pulses to turn on and off throughout the simulation. We can pass in
    the number of B1 pulses we want and and constant flip angle for each B1
    pulse.

--- precession.m
+++ precession.m
@@ -78,42 +78,66 @@ M_initial = [0; 0; 1];      % Magnetization at t = 0.
 M_origin = [0; 0; 0];       % Origin of Magnetization vector.
 M_eq = [0; 0; 1];           % Equilibrium Magnetization.
 scale = 1;
-T1 = 2;                     % T1 relaxation (s).
-T2 = 1;                     % T2 relaxation (2).
-viewRotatingFrame = false;  % View precession in rotating reference frame
+T1 = 3;                     % T1 relaxation (s).
+T2 = 2;                     % T2 relaxation (2).
+viewRotatingFrame = true;   % View precession in rotating reference frame
 relaxationOn = false;       % Enable T1/T2 Relaxation?
-viewElevation = 30;         % Elevation to view spins (degrees): view(~, viewElevation)
-traceSpinTip = false;       % Enable tracing of tip of magnetization vector.
+viewElevation = 0;         % Elevation to view spins (degrees): view(~, viewElevation)
+traceSpinTip = true;        % Enable tracing of tip of magnetization vector.
 
 % B1 parameters: B1(t) = B1cos(wt)i - B1sin(wt)j (left handed rotation)
-magB1x = 0.5;               % Magnitude of B1 x component
-magB1y = 0.5;               % Magnitude of B1 y component
-magB1z = 0;                 % Magnitude of B1 z component
+nTR = 6;
+B1mag = 4;                  % Magnitude of B1
 w1 = w;                     % Frequency of B1 pulse
 B1phase = pi/2;             % Phase of B1 (radians)
 B1phase_degrees = B1phase * (180/pi);
-B1x = @(t) magB1x*cos(w1*t + B1phase); % x component of B1
-B1y = @(t) magB1y*sin(w1*t + B1phase); % y component of B1
+B1x = @(t) B1mag*cos(w1*t + B1phase); % x component of B1
+B1y = @(t) B1mag*sin(w1*t + B1phase); % y component of B1
+T_90 = (pi/2) / (gamma*B1mag);
+T_180 = (pi/2) / (gamma*B1mag);
+flipAngle = pi/8;
 
-% Model
+% ODE initialization
 bloch = ode;
-if relaxationOn
-    bloch.ODEFcn = @(t,M) [gamma*B0*M(2) + gamma*M(3)*B1y(t) - M(1)/T2;
-                          -gamma*B0*M(1) + gamma*M(3)*B1x(t) - M(2)/T2;
-                          -gamma*M(1)*B1y(t) - gamma*M(2)*B1x(t) - (M(3) - M_eq(3))/T1];
-else 
-    bloch.ODEFcn = @(t,M) [gamma*B0*M(2) + gamma*M(3)*B1y(t);
-                          -gamma*B0*M(1) + gamma*M(3)*B1x(t);
-                          -gamma*M(1)*B1y(t) - gamma*M(2)*B1x(t) + 0*M(3)];
-end
-
+bloch.ODEFcn = @(t, M) blochODEFcn(t, M, gamma, B0, B1x, B1y, T1, T2, M_eq, relaxationOn, w1, B1mag, nTR, Tend, flipAngle);
 bloch.InitialTime = 0;
 bloch.InitialValue = M_initial; % Initial magnetization orientation. 
+function dMdt = blochODEFcn(t, M, gamma, B0, B1x, B1y, T1, T2, M_eq, relaxationOn, w1, B1mag, nTR, Tend, flipAngle)
+    % Calculate the time interval between excitations
+    TR_interval = Tend / nTR;
+    % Determine if t falls within an "on" phase of the B1 field
+    B1Duration = flipAngle/(gamma*B1mag); % Duration neded for flip Angle of theta radians.
+    % Modulo operation helps check the timing within each TR interval
+    if mod(t, TR_interval) < B1Duration
+        B1x_t = B1x(t);
+        B1y_t = B1y(t);
+    else
+        B1x_t = 0;
+        B1y_t = 0;
+    end
+
+    % Define the Bloch equations with or without relaxation
+    if relaxationOn
+        dMdt = [
+            gamma * B0 * M(2) + gamma * M(3) * B1y_t - M(1) / T2;
+            -gamma * B0 * M(1) + gamma * M(3) * B1x_t - M(2) / T2;
+            -gamma * M(1) * B1y_t - gamma * M(2) * B1x_t - (M(3) - M_eq(3)) / T1
+        ];
+    else
+        dMdt = [
+            gamma * B0 * M(2) + gamma * M(3) * B1y_t;
+            -gamma * B0 * M(1) + gamma * M(3) * B1x_t;
+            -gamma * M(1) * B1y_t - gamma * M(2) * B1x_t
+        ];
+    end
+end
+
+% Solve
 solution = solve(bloch, time);
 t = solution.Time;
 M = solution.Solution;
 
-% Plot x, y, and z components vs time.
+% Plot Magnetization components (x, y, and z) vs time.
 figure
 plot(t, M(1,:),'r','DisplayName',"Mx(t)");
 hold on;
@@ -124,8 +148,8 @@ legend();
 
 
 % 3D Plot and appearance.
-f = figure(2);
-f.Position = [10 10 1000 1000];
+fig = figure(2);
+fig.Position = [10 10 1000 1000];
 %figure('Renderer', 'painters', 'Position', [10 10 1000 1000])
 spinColors = [1 0 0]; 
 set(gcf, 'Color', 'black');
@@ -150,7 +174,7 @@ end
 
 % 3D Animation precession
 if viewRotatingFrame
-    if magB1x + magB1y ~= 0 % If we have a B1 field, align view along B1 axis
+    if B1mag ~= 0 % If we have a B1 field, align view along B1 axis
         initialView = B1phase_degrees - 90;
         view(initialView, viewElevation);
     else
@@ -163,7 +187,7 @@ end
 
 spin = showspins(M(:,1),scale,M_initial,spinColors);
 for i=1:length(t)
-    figure(f)
+    figure(fig)
     delete(spin)
     spin = showspins(M(:,i), scale, M_origin, spinColors);
     if viewRotatingFrame
@@ -175,6 +199,6 @@ for i=1:length(t)
         addpoints(spinTip, M(1,i), M(2,i), M(3,i));
     end
 
-    pause(0.01);
-    fprintf('time = %fs \n', i)
+    pause(0.001);
+    fprintf('time = %fs \n', t(i))
 end
